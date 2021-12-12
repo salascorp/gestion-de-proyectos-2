8ab5f09c5698fed8c6b95b0e17f72344ecd0f3d0
salascorp <salascorp@gmail.com>
2021-12-12 13:59:20 -0500

    agregar despliegue de dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
     <div className="bg-green-500">
       <p>Pagina de index</p>
       <Link href="/admin/usuarios/">
-        <a className='cursor-pointer'>Ir a admin usuarios de prueba</a>
+        <a className='cursor-pointer'>Ir a admin usuarios de prueba purba desarrollador</a>
       </Link>
     </div>
   )
